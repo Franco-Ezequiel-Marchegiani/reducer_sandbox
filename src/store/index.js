be4ffcd61f474c5313e94c03011e3e750be03c18
Franco Ezequiel <eze.cavs@hotmail.com>
2024-08-20 14:58:44 -0300

    Adding the alternative of ussing 'slice'

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import contadorReducer from '../reducers/contadorReducer';
+import { counterSlice } from '../reducers/contadorSlice';
 
 //Es una función con un objeto, le pasamos una configuración o reducer
 const store = configureStore({
     reducer:{
-        contador: contadorReducer,
-        //carrito: reducerCarrito,
+        //contador: contadorReducer,
+        contador: counterSlice,
 
     }
 })
